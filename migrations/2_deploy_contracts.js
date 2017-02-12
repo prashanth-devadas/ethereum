293f04e3e8ef86f2e23fbf9b1a3ff629ee3bd842
prashanth-devadas <prashanthharidas@yahoo.co.in>
2017-02-12 18:40:30 +0530

    Commented default solidity apps created by truffle

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,8 +3,8 @@ var MetaCoin = artifacts.require("./MetaCoin.sol");
 var HelloWorld = artifacts.require("./HelloWorld.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+//  deployer.deploy(ConvertLib);
+//  deployer.link(ConvertLib, MetaCoin);
+//  deployer.deploy(MetaCoin);
   deployer.deploy(HelloWorld);
 };
